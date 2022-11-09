ebb956bc89618b352fa42bcba8042978887a0843
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-09 14:18:32 +0100

    Added pyinstaller script

--- make_executable.sh
+++ make_executable.sh
@@ -1,7 +1,7 @@
 python3 -m ensurepip
 python3 -m pip install hvcc
 python3 -m pip install pyinstaller
-python3 $(python3 -m site --user-site)/PyInstaller/__main__.py -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
 mv ./dist/__init__ ./Heavy-$1
 rm -rf ./dist
 rm -rf ./build
