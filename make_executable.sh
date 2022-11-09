21e23cf2df2583e4250c9fdd2620762070a8503b
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-09 15:20:35 +0100

    Create univeral binary for MacOS

--- make_executable.sh
+++ make_executable.sh
@@ -1,7 +1,12 @@
 python3 -m ensurepip
 python3 -m pip install hvcc
 python3 -m pip install pyinstaller
+
+if [ "$(uname)" == "Darwin" ]; then
+python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+else
 python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+fi
 mv ./dist/__init__ ./Heavy-$1
 rm -rf ./dist
 rm -rf ./build
