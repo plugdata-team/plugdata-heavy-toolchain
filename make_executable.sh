6861d54b20f4fab118367a92fa10a33b007e6eaa
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-09 16:05:22 +0100

    Better OS detection

--- make_executable.sh
+++ make_executable.sh
@@ -2,12 +2,12 @@ python3 -m ensurepip
 python3 -m pip install hvcc
 python3 -m pip install pyinstaller
 
-if [ "$(uname)" == "Darwin" ]; then
-python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
-elif  [[ $(uname -s) == CYGWIN* ]]; then 
-python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*;./hvcc"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*;./hvcc"
 fi
 
 if  [[ $(uname -s) == CYGWIN* ]]; then 
