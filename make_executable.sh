98eb922e92e6cc633ee3b6da81c5bf804ce6a3e2
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-09 15:24:18 +0100

    Fix

--- make_executable.sh
+++ make_executable.sh
@@ -7,7 +7,7 @@ python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 -
 else
 python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
 fi
-mv ./dist/__init__ ./Heavy-$1
+mv ./dist/__init__ ./Heavy
 rm -rf ./dist
 rm -rf ./build
 rm -rf ./__init__.spec
