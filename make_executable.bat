8a27595d5d79c0769fdc7d2898ad4af05373fa93
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-10 18:37:59 +0100

    Fixed Windows build script

--- make_executable.bat
+++ make_executable.bat
@@ -4,9 +4,9 @@ python -m pip install pyinstaller
 
 FOR /F "tokens=*" %%g IN ('python -m site --user-site') do (SET PYTHON_SITE=%%g)
 
-python pyinstaller --noconfirm --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
+python pyinstaller -n Heavy --noconfirm --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
 
-move .\dist\__init__.exe ./Heavy.exe
+move .\dist\Heavy .\Heavy
 
 del /s /q .\dist\*
 del /s /q .\build\*
