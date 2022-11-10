9c1cf13103ee0f633b6d238e2a20b65406ec0be1
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-10 01:22:55 +0100

    Better strategy for Windows

---
+++ make_executable.bat
@@ -0,0 +1,13 @@
+python -m ensurepip
+python -m pip install hvcc
+python -m pip install pyinstaller
+
+FOR /F "tokens=*" %%g IN ('python -m site --user-site') do (SET PYTHON_SITE=%%g)
+
+python pyinstaller -F --noconfirm --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --add-data=".\hvcc/*;.\hvcc"
+
+move .\dist\__init__.exe ./Heavy.exe
+
+del /s /q .\dist\*
+del /s /q .\build\*
+del /s /q .\__init__.spec
--- make_executable.sh
+++ make_executable.sh
@@ -6,15 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data=".\hvcc/*;.\hvcc"
 fi
 
-if  [[ $(uname -s) == CYGWIN* ]]; then 
-mv ./dist/__init__.exe ./Heavy.exe
-else
 mv ./dist/__init__ ./Heavy
-fi
 rm -rf ./dist
 rm -rf ./build
 rm -rf ./__init__.spec
