c69f6c4d9269157f5afa3e43e9f1814288a51d13
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-10 15:57:54 +0100

    Fixed MacOS and Linux issues

--- make_executable.sh
+++ make_executable.sh
@@ -5,7 +5,7 @@ python3 -m pip install pyinstaller
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     python3 pyinstaller -F --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc"
+    python3 pyinstaller -F --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/*:./hvcc" --add-data="./hvcc/*:./"
 fi
 
 mv ./dist/__init__ ./Heavy
