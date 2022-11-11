75178715cc47502097dc4978228f49f85533cac8
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-11 14:35:01 +0100

    Install curl

--- scripts/build_heavy.bat
+++ scripts/build_heavy.bat
@@ -1,1 +1,13 @@
-./scripts/build_heavy.bat
+python -m ensurepip
+python -m pip install hvcc
+python -m pip install pyinstaller
+
+FOR /F "tokens=*" %%g IN ('python -m site --user-site') do (SET PYTHON_SITE=%%g)
+
+python pyinstaller -n Heavy --noconfirm --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --add-data=".\hvcc\hvcc\generators;.\generators" --add-data=".\hvcc\hvcc\core;.\hvcc\core" --add-data=".\hvcc\hvcc\generators;.\hvcc\generators" --add-data=".\hvcc\hvcc\interpreters;.\hvcc\interpreters"
+
+move .\dist\Heavy .\Heavy
+
+del /s /q .\dist\*
+del /s /q .\build\*
+del /s /q .\__init__.spec
