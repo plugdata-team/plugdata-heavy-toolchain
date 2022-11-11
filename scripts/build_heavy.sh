9269427e243275b0138e4eef938c8a1ba07c51d0
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-11 22:30:44 +0100

    Added missing dependencies

--- scripts/build_heavy.sh
+++ scripts/build_heavy.sh
@@ -5,9 +5,9 @@ python3 -m pip install hvcc
 python3 -m pip install pyinstaller
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    python3 pyinstaller -n Heavy --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
+    python3 pyinstaller -n Heavy --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --hidden-import json2daisy --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    python3 pyinstaller -n Heavy --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
+    python3 pyinstaller -n Heavy  --noconfirm --windowed --target-architecture universal2 --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --hidden-import json2daisy --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
 fi
 
 mv ./dist/Heavy ./Heavy
