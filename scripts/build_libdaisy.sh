c659531a0c1e8f473c3ab9665bf64b8ab43bc400
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-11 13:37:14 +0100

    Added Linux install for arm-none-eabi

--- scripts/build_libdaisy.sh
+++ scripts/build_libdaisy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p Heavy/usr/bin
+mkdir -p Heavy/usr/lib
+mkdir -p Heavy/usr/include
+
+cp $(which make) Heavy/usr/bin/make
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     
 # path variable
@@ -21,16 +27,33 @@ echo "Installing packages with Homebrew"
 brew install openocd dfu-util
 brew install $SCRIPTPATH/gcc-arm-embedded.rb --cask
 
-fi
-
-mkdir -p Heavy/usr/bin
-mkdir -p Heavy/usr/lib
-mkdir -p Heavy/usr/include
-
 cp $(which arm-none-eabi-gcc) Heavy/usr/bin/arm-none-eabi-gcc
 cp $(which dfu-util) Heavy/usr/bin/dfu-util
 cp $(which openocd) Heavy/usr/bin/openocd
-cp $(which make) Heavy/usr/bin/make
+
+else
+
+# https://askubuntu.com/a/1371525
+# https://developer.arm.com/downloads/-/gnu-rm
+
+VER=${VER:-'10.3-2021.10'}
+
+URL=https://developer.arm.com/-/media/Files/downloads/gnu/${VER}/binrel/gcc-arm-${VER}-x86_64-arm-none-eabi.tar.xz
+
+echo "Downloading arm-none-eabi-gcc"
+curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar "$URL"
+
+echo "Extracting..."
+mkdir tmp
+pushd tmp
+tar -xf ../gcc-arm-none-eabi.tar
+popd
+rm gcc-arm-none-eabi.tar
+
+mv tmp/gcc-arm-*/* gcc-arm-none-eabi/usr/
+
+fi
+
 
 cd ./libDaisy/
 make
