9aa72e5be31c25181510fd98a4a0f4a321382141
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-12 02:10:58 +0100

    Added ARM support for Linux, fixed compiler linking

--- scripts/build_libdaisy.sh
+++ scripts/build_libdaisy.sh
@@ -32,13 +32,14 @@ cp -f $(which openocd) Heavy/usr/bin/openocd
 
 else
 
-# https://askubuntu.com/a/1371525
-# https://developer.arm.com/downloads/-/gnu-rm
-
-URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-x86_64-arm-none-eabi.tar.xz?rev=bad6fbd075214a34b48ddbf57e741249&hash=F87A67141928852E079463E67E2B7A02"
+if [ $(uname -m) = "aarch64" ]; then
+    URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-aarch64-arm-none-eabi.tar.xz?rev=cd13d8fc408f42d680fcccc26281d945&hash=DD68E49B16AFE10346AE2B6D0AF4E23A"
+else
+    URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-x86_64-arm-none-eabi.tar.xz?rev=bad6fbd075214a34b48ddbf57e741249&hash=F87A67141928852E079463E67E2B7A02"
+fi
 
 echo "Downloading arm-none-eabi-gcc"
-#curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar.xz $URL
+curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar.xz $URL
 
 echo "Extracting..."
 mkdir tmp
@@ -50,11 +51,11 @@ rm gcc-arm-none-eabi.tar.xz
 cp -rf tmp/arm-gnu-*/* ./Heavy/usr/
 cp -f $(which make) Heavy/usr/bin/make
 
-sudo ln -s /usr/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
-sudo ln -s /usr/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
-sudo ln -s /usr/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
-sudo ln -s /usr/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
-sudo ln -s /usr/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
+sudo ln -s $(pwd)/Heavy/usr/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
+sudo ln -s $(pwd)/Heavy/usr/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
+sudo ln -s $(pwd)/Heavy/usr/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
+sudo ln -s $(pwd)/Heavy/usr/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
+sudo ln -s $(pwd)/Heavy/usr/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
 
 fi
 
