559893ff7f20b43d3ba2542a80206957ee79cd0c
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-12 01:38:22 +0100

    Make sure json2daisy files are copied

--- scripts/build_heavy.bat
+++ scripts/build_heavy.bat
@@ -4,7 +4,7 @@ python -m pip install pyinstaller
 
 FOR /F "tokens=*" %%g IN ('python -m site --user-site') do (SET PYTHON_SITE=%%g)
 
-python pyinstaller -n Heavy --noconfirm --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --add-data=".\hvcc\hvcc\generators;.\generators" --add-data=".\hvcc\hvcc\core;.\hvcc\core" --add-data=".\hvcc\hvcc\generators;.\hvcc\generators" --add-data=".\hvcc\hvcc\interpreters;.\hvcc\interpreters"
+python pyinstaller -n Heavy --noconfirm  --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --collect-data json2daisy --add-data=".\hvcc\hvcc\generators;.\generators" --add-data=".\hvcc\hvcc\core;.\hvcc\core" --add-data=".\hvcc\hvcc\generators;.\hvcc\generators" --add-data=".\hvcc\hvcc\interpreters;.\hvcc\interpreters"
 
 move .\dist\Heavy .\Heavy
 
