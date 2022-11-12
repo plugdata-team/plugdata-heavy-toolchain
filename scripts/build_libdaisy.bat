d94dfa77565857b2ffd83388b7724ce9bbe75423
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-12 18:48:15 +0100

    Fixed Windows expansion command

--- scripts/build_libdaisy.bat
+++ scripts/build_libdaisy.bat
@@ -1,7 +1,7 @@
 
 set URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-mingw-w64-i686-arm-none-eabi.zip?rev=02b9889af49c4da9bc47018c00e18eb5&hash=AE65D45D5C9377AC531CF2EDB447FA99"
 powershell -Command "Invoke-WebRequest %URL% -OutFile arm-none-eabi-gcc.zip"
-powershell -Command "arm-none-eabi-gcc.zip -DestinationPath .\tmp"
+powershell -Command "Expand-Archive arm-none-eabi-gcc.zip -DestinationPath .\tmp"
 
 del arm-none-eabi-gcc.zip
 
