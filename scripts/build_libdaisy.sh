6ada3e845f71bf4779f9713738c8dee1e640ed02
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-12 22:32:27 +0100

    Removed support for uncessesary boards in toolchain

--- scripts/build_libdaisy.sh
+++ scripts/build_libdaisy.sh
@@ -9,14 +9,14 @@ else
 fi
 
 echo "Downloading arm-none-eabi-gcc"
-curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar.xz $URL
+#curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar.xz $URL
 
 echo "Extracting..."
 mkdir tmp
 pushd tmp
-tar -xf ../gcc-arm-none-eabi.tar.xz
+#tar -xf ../gcc-arm-none-eabi.tar.xz
 popd
-rm gcc-arm-none-eabi.tar.xz
+#rm gcc-arm-none-eabi.tar.xz
 
 cp -rf tmp/arm-gnu-*/bin/* ./Heavy/bin
 cp -rf tmp/arm-gnu-*/lib ./Heavy
@@ -24,6 +24,16 @@ cp -rf tmp/arm-gnu-*/libexec ./Heavy
 cp -rf tmp/arm-gnu-*/share ./Heavy
 cp -rf tmp/arm-gnu-*/include ./Heavy
 cp -rf tmp/arm-gnu-*/arm-none-eabi ./Heavy
+
+mkdir -p "./Heavy/arm-none-eabi/lib/temp/"
+mv -f "./Heavy/arm-none-eabi/lib/thumb/v7e-m+dp" "./Heavy/arm-none-eabi/lib/temp" 
+rm -rf "./Heavy/arm-none-eabi/lib/thumb"
+mv -f "./Heavy/arm-none-eabi/lib/temp" "./Heavy/arm-none-eabi/lib/thumb"
+
+mkdir -p "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp"
+mv "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb/v7e-m+dp" "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp/v7e-m+dp"
+rm -rf "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb"
+mv "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp" "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb"
 
 cp -rf ./resources/heavy-static.a ./Heavy/lib/heavy-static.a
 cp -rf ./resources/daisy_makefile ./Heavy/share/daisy_makefile
