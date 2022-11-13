d0f5874998b3d00c13985f73e3b367756b915050
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-13 19:29:59 +0100

    Fix for batch script

--- scripts/build_libdaisy.bat
+++ scripts/build_libdaisy.bat
@@ -10,13 +10,13 @@ copy resources\daisy_makefile Heavy\share\daisy_makefile
 
 mkdir "Heavy\arm-none-eabi\lib\temp"
 move "Heavy\arm-none-eabi\lib\thumb\v7e-m+dp" "Heavy\arm-none-eabi\lib\temp\v7e-m+dp"
-del /S /Q "Heavy\arm-none-eabi\lib\thumb"
-rename "Heavy\arm-none-eabi\lib\temp" "Heavy\arm-none-eabi\lib\thumb"
+rmdir /S /Q "Heavy\arm-none-eabi\lib\thumb"
+rename "Heavy\arm-none-eabi\lib\temp" "thumb"
 
 mkdir "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp"
 move "\Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb\v7e-m+dp" "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp\v7e-m+dp"
-del /S /Q "Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb"
-rename "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp" "Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb"
+rmdir /S /Q "Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb"
+rename "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp" "thumb"
 
 del /S /Q ".\Heavy\arm-none-eabi\lib\arm"
 
