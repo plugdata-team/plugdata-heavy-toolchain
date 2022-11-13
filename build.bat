1cfa9d3a1676f2d65ea477ba9e064a74a945d79b
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-13 05:15:27 +0100

    Log version number into folder, for future proofing

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 call .\scripts\build_libdaisy.bat %1
 call .\scripts\build_heavy.bat
+
+echo v0.0.2> VERSION
--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 ./scripts/build_heavy.sh
 ./scripts/build_libdaisy.sh
+
+echo "v0.0.2" > VERSION
