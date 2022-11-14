2b3d43e421e5a5a6c8aa188da36cea1069bfcb28
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-14 19:38:41 +0100

    Cleaned up repo and build scrips

--- build.bat
+++ build.bat
@@ -1,4 +1,50 @@
-call .\scripts\build_libdaisy.bat %1
-call .\scripts\build_heavy.bat
+git clone --recursive https://github.com/electro-smith/DaisyToolchain
 
-echo v0.0.2> Heavy\VERSION
+set URL="https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/MinGit-2.38.1-64-bit.zip"
+
+move DaisyToolchain\windows Heavy
+copy resources\heavy-static.a Heavy\lib\heavy-static.a
+copy resources\daisy_makefile Heavy\share\daisy_makefile
+
+powershell -Command "Invoke-WebRequest %URL% -OutFile MinGit.zip"
+powershell -Command "Expand-Archive MinGit.zip -Force -DestinationPath .\tmp"
+
+move tmp\mingw64 Heavy\mingw64
+move tmp\etc Heavy\etc
+move tmp\usr Heavy\usr
+move tmp\cmd Heavy\cmd
+
+mkdir "Heavy\arm-none-eabi\lib\temp"
+move "Heavy\arm-none-eabi\lib\thumb\v7e-m+dp" "Heavy\arm-none-eabi\lib\temp\v7e-m+dp"
+rmdir /S /Q "Heavy\arm-none-eabi\lib\thumb"
+rename "Heavy\arm-none-eabi\lib\temp" "thumb"
+
+mkdir "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp"
+move "\Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb\v7e-m+dp" "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp\v7e-m+dp"
+rmdir /S /Q "Heavy\lib\gcc\arm-none-eabi\12.2.0\thumb"
+rename "Heavy\lib\gcc\arm-none-eabi\12.2.0\temp" "thumb"
+
+del /S /Q ".\Heavy\arm-none-eabi\lib\arm"
+
+cd libDaisy
+Heavy\bin\make.exe GCC_PATH=../Heavy/bin
+cd ..
+
+xcopy /E /H /C /I libDaisy Heavy\lib\libDaisy
+
+python -m ensurepip
+python -m pip install hvcc
+python -m pip install pyinstaller
+
+FOR /F "tokens=*" %%g IN ('python -m site --user-site') do (SET PYTHON_SITE=%%g)
+
+python resources\pyinstaller -n Heavy --noconfirm  --windowed --paths %PYTHON_SITE% .\hvcc\hvcc\__init__.py --collect-data json2daisy --add-data=".\hvcc\hvcc\generators;.\generators" --add-data=".\hvcc\hvcc\core;.\hvcc\core" --add-data=".\hvcc\hvcc\generators;.\hvcc\generators" --add-data=".\hvcc\hvcc\interpreters;.\hvcc\interpreters"
+
+copy .\dist\Heavy\json2daisy\resources\component_defs.json .\dist\Heavy\json2daisy\resources\seed.json
+move .\dist\Heavy .\Heavy\bin\
+
+del /s /q .\dist\*
+del /s /q .\build\*
+del /s /q .\__init__.spec
+
+cp VERSION Heavy\VERSION
--- build.sh
+++ build.sh
@@ -1,6 +1,94 @@
 #!/bin/bash
 
-./scripts/build_heavy.sh
-./scripts/build_libdaisy.sh
+#!/bin/bash
 
-echo "v0.0.2" > ./Heavy/VERSION
+# Download arm compiler for compiling on daisy
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-darwin-x86_64-arm-none-eabi.tar.xz"
+elif [[ $(uname -m) == "aarch64" ]]; then
+    URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-aarch64-arm-none-eabi.tar.xz"
+else
+    URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-bet1/binrel/arm-gnu-toolchain-12.2.mpacbti-bet1-x86_64-arm-none-eabi.tar.xz"
+fi
+
+echo "Downloading arm-none-eabi-gcc"
+curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar.xz $URL
+
+echo "Extracting..."
+mkdir tmp
+pushd tmp
+tar -xf ../gcc-arm-none-eabi.tar.xz
+popd
+rm gcc-arm-none-eabi.tar.xz
+
+mkdir Heavy
+cp -rf tmp/arm-gnu-*/bin ./Heavy
+cp -rf tmp/arm-gnu-*/lib ./Heavy
+cp -rf tmp/arm-gnu-*/libexec ./Heavy
+cp -rf tmp/arm-gnu-*/share ./Heavy
+cp -rf tmp/arm-gnu-*/include ./Heavy
+cp -rf tmp/arm-gnu-*/arm-none-eabi ./Heavy
+
+# Reduce package size by only including the daisy platform tools
+mkdir -p "./Heavy/arm-none-eabi/lib/temp/"
+mv -f "./Heavy/arm-none-eabi/lib/thumb/v7e-m+dp" "./Heavy/arm-none-eabi/lib/temp" 
+rm -rf "./Heavy/arm-none-eabi/lib/thumb"
+mv -f "./Heavy/arm-none-eabi/lib/temp" "./Heavy/arm-none-eabi/lib/thumb"
+
+mkdir -p "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp"
+mv "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb/v7e-m+dp" "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp/v7e-m+dp"
+rm -rf "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb"
+mv "./Heavy/lib/gcc/arm-none-eabi/12.2.0/temp" "./Heavy/lib/gcc/arm-none-eabi/12.2.0/thumb"
+
+rm -rf "./Heavy/arm-none-eabi/lib/arm"
+
+# copy a prebuild static library for heavy
+cp -rf ./resources/heavy-static.a ./Heavy/lib/heavy-static.a
+cp -rf ./resources/daisy_makefile ./Heavy/share/daisy_makefile
+
+# build a version of GNU make that has no dependencies
+pushd resources/unix_make
+tar -xf make-4.4.tar.gz
+pushd make-4.4
+chmod +x ./build.sh
+chmod +x ./configure
+
+# Hack: make sure libintl is not found on macOS, when building on Github actions server!
+if [[ "$OSTYPE" == "darwin"* ]] && $GITHUB_ACTIONS_BUILD; then
+rm -f /usr/local/opt/gettext/lib/libintl*.dylib
+fi
+
+./configure --disable-dependency-tracking --with-guile=no --without-libintl-prefix
+./build.sh
+cp make ../../../Heavy/bin/make
+popd
+popd
+
+# Pre-build libdaisy
+pushd libDaisy
+make GCC_PATH=../Heavy/bin/
+popd
+
+cp -rf ./libDaisy ./Heavy/lib/libDaisy
+
+
+# Package Heavy with pyinstaller
+python3 -m ensurepip
+python3 -m pip install hvcc
+python3 -m pip install pyinstaller
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    python3 ./resources/pyinstaller -n Heavy --noconfirm --windowed --paths $(python3 -m site --user-site) ./hvcc/hvcc/__init__.py --collect-data json2daisy --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    python3 ./resources/pyinstaller -n Heavy --noconfirm --windowed --paths $(python3 -m site --user-site) --target-architecture universal2 ./hvcc/hvcc/__init__.py --collect-data json2daisy --add-data="./hvcc/hvcc/generators:./generators" --add-data="./hvcc/hvcc/core:./hvcc/core" --add-data="./hvcc/hvcc/generators:./hvcc/generators" --add-data="./hvcc/hvcc/interpreters:./hvcc/interpreters"
+fi
+
+cp ./dist/Heavy/json2daisy/resources/component_defs.json ./dist/Heavy/json2daisy/resources/seed.json
+
+mv ./dist/Heavy Heavy/bin/Heavy
+
+rm -rf ./dist
+rm -rf ./build
+rm -rf ./Heavy.spec
+
+cp VERSION ./Heavy/VERSION
