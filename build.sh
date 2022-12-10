8a6eb6331238cab87890707c12d2a87eedd4113a
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-12-10 04:24:59 +0100

    Re-added DPF

--- build.sh
+++ build.sh
@@ -121,7 +121,7 @@ make GCC_PATH=../Heavy/bin/
 popd
 
 cp -rf ./libDaisy ./Heavy/lib/libDaisy
-cp -rf ./dpf ./Heavy/lib/dpf
+cp -rf ./DPF ./Heavy/lib/dpf
 
 # Package Heavy with pyinstaller
 python3 -m ensurepip
