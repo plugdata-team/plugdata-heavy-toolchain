d47028aa92cc790e86f9b6437aa79fe4aa2887fe
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-12-10 23:54:34 +0100

    undo last commit

--- build.sh
+++ build.sh
@@ -116,10 +116,6 @@ pushd make-4.4
 chmod +x ./build.sh
 chmod +x ./configure
 
-# On Linux, enter our build-anywhere environment to build a cross-distro version of make
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    source ../Heavy/scripts/anywhere-setup.sh
-fi
 
 # Hack: make sure libintl is not found on macOS when building on Github actions server!
 if [[ "$CLEAR_INTL" == "1" ]]; then
@@ -131,11 +127,6 @@ fi
 cp make ../Heavy/bin/make
 popd
 rm -rf make-4.4 make-4.4.tar.gz
-
-# Exit build-anywhere environment
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-source ~/.bashrc
-fi
 
 # Pre-build libdaisy
 pushd libDaisy
