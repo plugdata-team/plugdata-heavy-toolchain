e21b8ceefb4021b4dc7d52a7f01b73e679bffc8d
dreamer <dreamer@puikheid.nl>
2026-08-12 15:59:53 +0200

    copy usb_driver to current incorrect path in plugdata

--- build.bat
+++ build.bat
@@ -14,6 +14,8 @@ copy .\resources\terrarium.json .\Heavy\usr\etc\terrarium.json
 copy .\resources\versio.json .\Heavy\usr\etc\versio.json
 copy .\resources\hothouse.json .\Heavy\usr\etc\hothouse.json
 xcopy /E /H /C /I resources\usb_driver Heavy\usr\etc\usb_driver
+:: fix issue in current plugdata
+xcopy /E /H /C /I resources\usb_driver Heavy\etc\usb_driver
 
 del /S /Q ".\Heavy\usr\arm-none-eabi\lib\arm"
 
